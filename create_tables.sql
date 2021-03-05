f08fa39072242617b9f349d3363f2d75fe168396
duncan <duncan@duncan.duncan>
2021-03-05 12:32:30 +0000

    Create test and prod tables since we are using one DB

--- create_tables.sql
+++ create_tables.sql
@@ -10,6 +10,25 @@ CREATE TABLE stock_history(
     dv INT,
     pe INT,
     symbol VARCHAR,
+    company_name VARCHAR,
+    company_desc VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE test(
+    id SERIAL PRIMARY KEY,
+    cp INT,
+    op INT,
+    bv INT,
+    bu INT,
+    sb INT,
+    yd INT,
+    om INT,
+    dv INT,
+    pe INT,
+    symbol VARCHAR,
+    company_name VARCHAR,
+    company_desc VARCHAR,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
